e093f195b8c2d0638b3d399449c9a21e6e167331
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2022-02-04 12:49:07 +0530

    revert

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 import logging
 import discord
 from discord.ext import commands
-import Music
 
-cogs = [music]
-
-client = commands.Bot(command_prefix = '?', intents = discord.Intents.all())
-
-for i in range(len(cogs)):
-    cogs[i].setup(client)
+client = commands.Bot(command_prefix = '$')
 
 
 client.run("OTM4NzI2NzA1NDI0NjQ2MjA0.YfufXw.KJtG08ZgjTdbH2CkhlYpr-PgNSo")
