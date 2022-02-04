32f0dd764c5d64e5aa870e9277e5ac918364add1
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2022-02-04 10:26:13 +0530

    added logging and ping, migrated to nextcord

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
+#imp---
 import logging
-import discord
+import nextcord
+from nextcord.ext import commands
+#---
 
+client = commands.Bot(command_prefix="$") # custom prefix command?
 
-client = discord.Client()
+@client.command()
+async def ping(ctx):
+    latency = client.latency
+    await ctx.send(f"Numerical latency: {latency} ms")
 
 @client.event
 async def on_ready():
-    print("LOL")
+    print("")
 
+    
 client.run("OTM4NzI2NzA1NDI0NjQ2MjA0.YfufXw.KJtG08ZgjTdbH2CkhlYpr-PgNSo")
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s-%(message)s")
