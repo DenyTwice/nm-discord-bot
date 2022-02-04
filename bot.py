6c388f6a61a483062b49a2977400c1a27664a903
Kurian Tony <88186087+quiricus@users.noreply.github.com>
2022-02-04 10:35:35 +0530

    Blah
    blahi

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import logging
 import discord
+from discord.ext import commands
+import Music
 
+cogs = [music]
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '?', intents = discord.Intents.all())
 
-@client.event
-async def on_ready():
-    print("LOL")
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
 
 client.run("OTM4NzI2NzA1NDI0NjQ2MjA0.YfufXw.KJtG08ZgjTdbH2CkhlYpr-PgNSo")
+
