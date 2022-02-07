42a9a2135d6f70978bdc3c6552a0b5d97c02ef93
LiquidFlare <46068905+LiquidFlare@users.noreply.github.com>
2022-02-07 11:46:57 +0530

    button for portions
    hmhmhmhm

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import logging
 import nextcord
 from nextcord.ext import commands
+from nextcord.ui import Button, View
 import os
 from dotenv import load_dotenv
 #---
@@ -18,9 +19,20 @@ async def ping(ctx):
     await ctx.send(f"Numerical latency: {latency} ms")
 
 
+@client.command()
+async def butt(ctx):
+    button = Button(label="Portions", style=nextcord.ButtonStyle.blurple)
+    async def button_callback(interaction):
+        await interaction.response.edit_message(content="Physics: Ray Optics, Wave Optics\nChemistry: Amines, Aldehydes, Electrochemistry\nEnglish:\nComputer: Data Structures, Computer Networks, SQL with Python\nMaths: Integrals, Probability ")
+    button.callback = button_callback
+    view = View()
+    view.add_item(button)
+    await ctx.send("Slave reporting for duty!", view=view)
+    
+
 @client.event
 async def on_ready():
     logging.info("Bot online")
 
-
+    
 client.run(os.getenv("DISCORD_TOKEN"))
